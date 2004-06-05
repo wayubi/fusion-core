dfde37a8c147832f9189772925b49a68009405bc
W. Latif Ayubi <wayubi@gmail.com>
2004-06-05 15:45:09 +0000

    - 06-05-04: Added Skill Failure error checking to PSS. [AlexKreuz]
    git-svn-id: svn+ssh://svn.code.sf.net/p/fusion/code/core/trunk@744 a2e91628-aa1e-412e-9966-34abe3d8d708

--- Core/src/FEATURES/Player_Skills.pas
+++ Core/src/FEATURES/Player_Skills.pas
@@ -8,17 +8,33 @@ uses
     procedure parse_skills(tc : TChara);
     procedure process_effect(tc : TChara; success : Integer);
 
-	function skill_provoke(tc : TChara) : Word;
+	function skill_provoke(tc : TChara) : Integer;
 
 implementation
 
 	procedure parse_skills(tc : TChara);
     var
-    	success : Word;
+    	success : Integer;
     begin
     	case tc.MSkill of
         	6: success := skill_provoke(tc);
         end;
+
+        {
+	        Success Codes:
+    	    -1 : Success
+        	0 : Skill Failed
+	        1 : Not enough SP
+    	    2 : Not enough HP
+	        3 : No memo
+    	    4 : In cast delay
+	        5 : Not enough money
+    	    6 : Weapon is not usable with the skill
+	        7 : No red gemstone
+    	    8 : No blue gemstone
+	        9 : No yellow gemstone
+    	    a : Skill has failed
+        }
 
         process_effect(tc, success);
     end;
@@ -31,12 +47,21 @@ implementation
     begin
         tm := Map.Objects[Map.IndexOf(tc.Map)] as TMap;
 
-    	WFIFOW( 0, $011a);
-	    WFIFOW( 2, tc.MSkill);
-    	WFIFOW( 4, tc.MUseLV);
-	    WFIFOL( 6, tc.MTarget);
-    	WFIFOL(10, tc.ID);
-	    WFIFOB(14, success);
+        if (success = -1) then begin
+	    	WFIFOW( 0, $011a);
+		    WFIFOW( 2, tc.MSkill);
+    		WFIFOW( 4, tc.MUseLV);
+		    WFIFOL( 6, tc.MTarget);
+	    	WFIFOL(10, tc.ID);
+		    WFIFOB(14, 1);
+        end else begin
+        	WFIFOW( 0, $0110);
+        	WFIFOW( 2, tc.MSkill);
+        	WFIFOW( 4, 0);
+        	WFIFOW( 6, 0);
+        	WFIFOB( 8, 0);
+        	WFIFOB( 9, success);
+        end;
 
         if tc.MTargetType = 0 then begin
         	ts := TMob.Create;
@@ -49,40 +74,57 @@ implementation
         end;
     end;
 
-	function skill_provoke(tc : TChara) : Word;
+	function skill_provoke(tc : TChara) : Integer;
     var
     	ts : TMob;
         tc1 : TChara;
         tm : TMap;
         tl : TSkillDB;
+        rand : Integer;
     begin
-    	Result := 1;
+    	Result := -1;
         
         tm := Map.Objects[Map.IndexOf(tc.Map)] as TMap;
-        tL := tc.Skill[tc.MSkill].Data;
+        tl := tc.Skill[tc.MSkill].Data;
 
-        if tc.MTargetType = 0 then begin
-        	ts := TMob.Create;
-    		ts := tc.AData;
+        rand := Random(100);
+        if ( (tc.Skill[tc.MSkill].Lv * 3 + 50) >= rand ) then begin
+        	{ Provoke Successful. % chance checked. } 
+	        if tc.MTargetType = 0 then begin
+    	    	ts := TMob.Create;
+    			ts := tc.AData;
 
-    		if ts.Data.Race <> 1 then begin
-        		ts.ATKPer := word(tl.Data1[tc.MUseLV]);
-        		ts.DEFPer := word(tl.Data2[tc.MUseLV]);
-		    end else begin
-				Result := 0;
-    		end;
+	    		if ( (ts.Data.Race <> 1) and (ts.Data.MEXP = 0) ) then begin
+    	        	{ PvM: Provoke Succcessful. No Undead or MVP. }
+        			ts.ATKPer := word(tl.Data1[tc.MUseLV]);
+        			ts.DEFPer := word(tl.Data2[tc.MUseLV]);
+					ts.ATarget := tc.ID;
+					ts.ARangeFlag := false;
+					ts.AData := tc;
+			    end else begin
+            		{ PvM: Provoke Failed. Wrong Race/Type. }
+					Result := 0;
+	    		end;
+    	    end else begin
+        		{ PvP: Provoke Successful }
+        		tc1 := TChara.Create;
+	    		tc1 := tc.AData;
+    	        tc1.ATarget := tc.ID;
+        	    tc1.AData := tc;
+                tc1.AMode := 2;
+
+                tc1.NextPoint := tc.Point;
+                tc1.NextFlag := True;
+
+                { Alex: Now I just need to get tc1 to attack tc }
+
+    			tc1.DamageFixS[1] := word(tl.Data1[tc.MUseLV]);
+    			tc1.DEF1 := word(tl.Data2[tc.MUseLV]) * tc1.DEF1 div 100;
+            end;
         end else begin
-        	tc1 := TChara.Create;
-    		tc1 := tc.AData;
-            { Alex: A lot of these PVP calculations are a complete mess. Fusion looks
-            awesome on PVM, but try PVP and you'll see a totally different story.
-            I don't even know why Provoke is calculating damage, but obviously these
-            need to be double checked one by one. }
-    		//frmMain.DamageCalc3(tm, tc, tc1, Tick, 0, tl.Data1[MUseLV], tl.Element, tl.Data2[MUseLV]);
-    		tc1.DamageFixS[1] := word(tl.Data1[tc.MUseLV]); // ATK Increase upon the medium size to ease the pain.
-    		tc1.DEF1 := word(tl.Data2[tc.MUseLV]) * tc1.DEF1 div 100;
+        	{ Provoke failed. % chance too low. }
+            Result := 1;
         end;
-
     end;
 
 end.
