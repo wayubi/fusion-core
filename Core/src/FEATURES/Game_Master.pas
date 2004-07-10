584fae10ca32bdd61d34499625a6c47f9c99b56a
Trihan <Trihan@fusion.project>
2004-07-10 10:42:17 +0000

    - 07-10-04: Added @int to ALBGM. [Trihan]
    git-svn-id: svn+ssh://svn.code.sf.net/p/fusion/code/core/trunk@907 a2e91628-aa1e-412e-9966-34abe3d8d708

--- Core/src/FEATURES/Game_Master.pas
+++ Core/src/FEATURES/Game_Master.pas
@@ -109,6 +109,7 @@ var
     GM_ATHENA_STR : Byte;
     GM_ATHENA_AGI : Byte;
     GM_ATHENA_VIT : Byte;
+    GM_ATHENA_INT : Byte;
 
 
     GM_Access_DB : TIntList32;
@@ -219,6 +220,7 @@ var
     function command_athena_str(tc : TChara; str : String) : String;
     function command_athena_agi(tc : TChara; str : String) : String;
     function command_athena_vit(tc : TChara; str : String) : String;
+    function command_athena_int(tc : TChara; str : String) : String;
 
 implementation
 
@@ -334,6 +336,7 @@ implementation
         GM_ATHENA_STR := StrToIntDef(sl.Values['ATHENA_STR'], 1);
         GM_ATHENA_AGI := StrToIntDef(sl.Values['ATHENA_AGI'], 1);
         GM_ATHENA_VIT := StrToIntDef(sl.Values['ATHENA_VIT'], 1);
+        GM_ATHENA_INT := StrToIntDef(sl.Values['ATHENA_INT'], 1);
 
         sl.Free;
         ini.Free;
@@ -454,6 +457,7 @@ Called when we're shutting down the server *only*
         ini.WriteString('Athena GM Commands', 'ATHENA_STR', IntToStr(GM_ATHENA_STR));
         ini.WriteString('Athena GM Commands', 'ATHENA_AGI', IntToStr(GM_ATHENA_AGI));
         ini.WriteString('Athena GM Commands', 'ATHENA_VIT', IntToStr(GM_ATHENA_VIT));
+        ini.WriteString('Athena GM Commands', 'ATHENA_INT', IntToStr(GM_ATHENA_INT));
 
         ini.Free;
 
@@ -578,6 +582,7 @@ Called when we're shutting down the server *only*
             else if ( (copy(str, 1, length('str')) = 'str') and (check_level(tc.ID, GM_ATHENA_STR)) ) then error_msg := command_athena_str(tc, str)
             else if ( (copy(str, 1, length('agi')) = 'agi') and (check_level(tc.ID, GM_ATHENA_AGI)) ) then error_msg := command_athena_agi(tc, str)
             else if ( (copy(str, 1, length('vit')) = 'vit') and (check_level(tc.ID, GM_ATHENA_VIT)) ) then error_msg := command_athena_vit(tc, str)
+            else if ( (copy(str, 1, length('int')) = 'int') and (check_level(tc.ID, GM_ATHENA_INT)) ) then error_msg := command_athena_int(tc, str)
         end else if gmstyle = '/' then begin
         	if ( (aegistype = 'B') and (Copy(str, 1, length(tc.Name) + 2) = (tc.Name + ': ')) and (not (Copy(str, 1, 4) = 'blue') ) and (check_level(tc. ID, GM_AEGIS_B)) ) then error_msg := command_aegis_b(str)
             else if ( (aegistype = 'B') and (Copy(str, 1, length(tc.Name) + 2) <> (tc.Name + ': ')) and (not (Copy(str, 1, 4) = 'blue') ) and (check_level(tc. ID, GM_AEGIS_NB)) ) then error_msg := command_aegis_nb(str)
@@ -4176,6 +4181,46 @@ Called when we're shutting down the server *only*
 
             if (i > 0) then Result := 'GM_ATHENA_VIT Success. ' + IntToStr(tc.ParamBase[2] - oldpoints) + ' points added.'
             else Result := 'GM_ATHENA_VIT Success. ' + IntToStr(oldpoints - tc.ParamBase[2]) + ' points subtracted.'
+        end else begin
+            Result := Result + 'Point amount out of range [-32767-32767].';
+        end;
+    end;
+
+    function command_athena_int(tc : TChara; str : String) : String;
+    var
+        i, k, oldpoints : Integer;
+    begin
+        Result := 'GM_ATHENA_INT Failure.';
+
+        oldpoints := tc.ParamBase[3];
+
+        Val(Copy(str, 5, 256), i, k);
+        if (k = 0) and (i >= -32767) and (i <= 32767) then begin
+            if i < 0 then begin
+                if (tc.ParamBase[3] + i >= 1) then tc.ParamBase[3] := tc.ParamBase[3] + i
+                else if (tc.ParamBase[3] + i < 1) and (tc.ParamBase[3] > 1) then begin
+                    tc.ParamBase[3] := 1;
+                end else begin
+                    Result := Result + 'Minimum number of points is 1.';
+                    Exit;
+                end;
+            end
+
+            else begin
+                if (tc.ParamBase[3] + i <= 32767) then tc.ParamBase[3] := tc.ParamBase[3] + i
+                else if (tc.ParamBase[3] + i > 32767) and (tc.ParamBase[3] < 32767) then begin
+                    tc.ParamBase[3] := 32767;
+                end else begin
+                    Result := Result + 'Maximum number of points is 32767.';
+                    Exit;
+                end;
+            end;
+
+            CalcStat(tc);
+            SendCStat(tc);
+
+            if (i > 0) then Result := 'GM_ATHENA_INT Success. ' + IntToStr(tc.ParamBase[3] - oldpoints) + ' points added.'
+            else Result := 'GM_ATHENA_INT Success. ' + IntToStr(oldpoints - tc.ParamBase[3]) + ' points subtracted.'
         end else begin
             Result := Result + 'Point amount out of range [-32767-32767].';
         end;
