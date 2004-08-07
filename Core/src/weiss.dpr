af23fa34e2f43486a4718c33fd8d3e59ae9c5a4a
tsusai <tsusai@fusion.project>
2004-08-07 14:27:49 +0000

    - 08-07-04: Updated weiss.dpr for linux directory structure. [Tsusai]
    git-svn-id: svn+ssh://svn.code.sf.net/p/fusion/code/core/trunk@950 a2e91628-aa1e-412e-9966-34abe3d8d708

--- Core/src/weiss.dpr
+++ Core/src/weiss.dpr
@@ -6,10 +6,6 @@ uses
   {$IFDEF MSWINDOWS}
   madExcept,
   Forms,
-  {$ENDIF}
-  {$IFDEF LINUX}
-  QForms,
-  {$ENDIF}
 
   Main in 'CORE\Main.pas' {frmMain},
   Common in 'CORE\Common.pas',
@@ -44,6 +40,45 @@ uses
   Zip in '3RDPARTY\Zip.pas',
   ZipDlls in '3RDPARTY\ZipDlls.pas',
   List32 in '3RDPARTY\List32.pas';
+  {$ENDIF}
+
+  {$IFDEF LINUX}
+  QForms,
+
+  Main in 'CORE/Main.pas' {frmMain},
+  Common in 'CORE/Common.pas',
+  Globals in 'CORE/Globals.pas',
+
+  Login in 'ZONE/Login.pas',
+  CharaSel in 'ZONE/CharaSel.pas',
+  Game in 'ZONE/Game.pas',
+
+  Database in 'DATABASE/Database.pas',
+  PlayerData in 'DATABASE/PlayerData.pas',
+
+  FusionSQL in 'SQL/FusionSQL.pas',
+  SQLData in 'SQL/SQLData.pas',
+
+  MonsterAI in 'AI/MonsterAI.pas',
+  Path in 'AI/Path.pas',
+
+  Game_Master in 'FEATURES/Game_Master.pas',
+  JCon in 'FEATURES/JCon.pas',
+  Player_Skills in 'FEATURES/Player_Skills.pas',
+  WeissINI in 'FEATURES/WeissINI.pas',
+
+  GlobalLists in 'OLD/GlobalLists.pas',
+  Manifest in 'OLD/Manifest.pas',
+  PacketProcesses in 'OLD/PacketProcesses.pas',
+  Script in 'OLD/Script.pas',
+  Skills in 'OLD/Skills.pas',
+  Skill_Constants in 'OLD/Skill_Constants.pas',
+  TrimStr in 'OLD/TrimStr.pas',
+
+  Zip in '3RDPARTY/Zip.pas',
+  ZipDlls in '3RDPARTY/ZipDlls.pas',
+  List32 in '3RDPARTY/List32.pas';
+  {$ENDIF}
 
 {$R *.res}
 
