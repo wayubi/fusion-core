7bcb4bf87e25188df38ff741e9d3b51e91126b35
W. Latif Ayubi <wayubi@gmail.com>
2005-10-17 08:12:44 +0000

    - 10-17-05: ADODB account data loading completed. [AlexKreuz]
    git-svn-id: svn+ssh://svn.code.sf.net/p/fusion/code/core/trunk@1324 a2e91628-aa1e-412e-9966-34abe3d8d708

--- Core/src/DATABASE/ADODB_LOAD_ACCOUNTS.pas
+++ Core/src/DATABASE/ADODB_LOAD_ACCOUNTS.pas
@@ -15,33 +15,45 @@ uses
     procedure adodb_load_account(UID : String = '*');
     var
       sqlCommand : string;
+      tp : TPlayer;
     begin
 
-        frmMain.ADODataSet1.CommandText := 'select * from login;';
-        frmMain.ADODataSet1.Open;
+      if (UID = '*') then begin
+        sqlCommand := 'select account_id, userid, user_pass, email, sex, level, ban_until from login;';
+      end else begin
+        sqlCommand := 'select account_id, userid, user_pass, email, sex, level, ban_until from login where userid = ''' +UID+ ''';';
+      end;
 
-        debugout.Lines.Add('Loading eAthena SQL login table');
-        while not frmMain.ADODataSet1.Eof do begin
-          debugout.Lines.Add(frmMain.ADODataSet1.Fields[1].CurValue);
-          frmMain.ADODataSet1.Next;
+      frmMain.ADODataSet1.CommandText := sqlCommand;
+      frmMain.ADODataSet1.Open;
+
+      debugout.Lines.Add('Loading eAthena SQL login table');
+
+      while not frmMain.ADODataSet1.Eof do begin
+
+        if not (frmMain.ADODataSet1.FieldByName('sex').CurValue = 'S') then begin
+          tp := TPlayer.Create;
+
+          tp.ID := frmMain.ADODataSet1.FieldByName('account_id').CurValue;
+          tp.Name := frmMain.ADODataSet1.FieldByName('userid').CurValue;
+          tp.Pass := frmMain.ADODataSet1.FieldByName('user_pass').CurValue;
+          tp.Mail := frmMain.ADODataSet1.FieldByName('email').CurValue;
+          tp.AccessLevel := frmMain.ADODataSet1.FieldByName('level').CurValue;
+
+          if (frmMain.ADODataSet1.FieldByName('sex').CurValue = 'M') then tp.Gender := 1
+          else if (frmMain.ADODataSet1.FieldByName('sex').CurValue = 'F') then tp.Gender := 0;
+
+          tp.Banned := False;
+          
+          PlayerName.AddObject(tp.Name, tp);
+          Player.AddObject(tp.ID, tp);
+
+          //tp := Player.Objects[Player.IndexOf(frmMain.ADODataSet1.FieldByName('account_id').CurValue)] as TPlayer;
+          //debugout.Lines.Add('Player '+tp.Name+' was loaded successfully.');
         end;
 
-        
-       {
-       while not frmMain.ADODataSet1.Eof do begin
-        debugout.Lines.Add(frmMain.ADODataSet1.Fields[0].CurValue);
         frmMain.ADODataSet1.Next;
-       end;
-        }
-
-       {
-        ADODataSet1.CommandText := 'show databases;';
-        ADODataSet1.Open;
-        debugout.Lines.Add(ADODataSet1.Fields[0].CurValue);
-        }
-
-
-
+      end;
 
     end;
     
